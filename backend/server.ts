d6127d037639b9319a3384873453c1c00462f5b9
Greg <harrison.greg.c@gmail.com>
2024-01-04 09:28:59 -0500

    still having issues getting a result from gpt4all :[

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,38 @@
-import express from 'express';
+// import express, { Request, Response } from 'express';
+// import bodyParser from 'body-parser';
+import { createCompletion, loadModel, PromptMessage } from 'gpt4all';
 
-const app = express();
-const port = 8080;
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// const jsonParser = bodyParser.json();
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+const initModel = async (name: string, options = {}) => {
+    const model = await loadModel(name, { ...options }); 
+
+    return model;
+}
+
+const start = async (modelName: string, message: string) => {
+    const options = { verbose: true };
+    const model = await initModel(modelName, options);
+
+    const messages: PromptMessage[] = [
+        { role: 'user', content: message }
+    ];
+
+    const response = await createCompletion(model, messages);
+
+    return response;
+}
+
+start('mistral-7b-openorca.Q4_0.gguf', 'Please write a haiku about the sun').then(console.log);
+
+// app.post('/generate', jsonParser, async (req: Request, res: Response) => {
+    // const { modelName, messages, options } = req.body;
+    // res.json(response);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}...`);
+// });
