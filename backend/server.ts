77894bbc10382072e412b462d224543de2684669
Greg <harrison.greg.c@gmail.com>
2024-01-04 11:54:05 -0500

    IT LIVES

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,4 @@
-// import express, { Request, Response } from 'express';
-// import bodyParser from 'body-parser';
 import { createCompletion, loadModel, PromptMessage } from 'gpt4all';
-
-// const app = express();
-// const port = 3000;
-
-// const jsonParser = bodyParser.json();
 
 const initModel = async (name: string, options = {}) => {
     const model = await loadModel(name, { ...options }); 
@@ -18,21 +11,15 @@ const start = async (modelName: string, message: string) => {
     const model = await initModel(modelName, options);
 
     const messages: PromptMessage[] = [
+        { role: 'system', content: 'You are a flavor scientist'},
         { role: 'user', content: message }
     ];
 
     const response = await createCompletion(model, messages);
 
-    return response;
+    model.dispose();
+
+    return response.choices[0].message;
 }
 
-start('mistral-7b-openorca.Q4_0.gguf', 'Please write a haiku about the sun').then(console.log);
-
-// app.post('/generate', jsonParser, async (req: Request, res: Response) => {
-    // const { modelName, messages, options } = req.body;
-    // res.json(response);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}...`);
-// });
+start('mistral-7b-openorca.Q4_0.gguf', 'write a haiku about hi-chew').then(console.log);
