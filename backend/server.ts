0d151f38047a6fc5beeea27f348305710c22e4b6
Greg <harrison.greg.c@gmail.com>
2024-01-09 15:03:55 -0500

    Created Regin model

--- backend/server.ts
+++ backend/server.ts
@@ -6,16 +6,22 @@ const initModel = async (name: string, options = {}) => {
     return model;
 }
 
-const start = async (modelName: string) => {
+const start = async (modelName: string, userMessage?: string) => {
     if (!Array.isArray(models.models)) throw new Error('Models must be an array of objects');
 
     const modelConfig = models.models.find((model) => model.name === modelName);
 
     if (!modelConfig) throw new Error(`Model ${modelName} not found`);
 
-    const {model, messages, initOptions, completionOptions} = modelConfig;
+    const {name, model, messages, initOptions, completionOptions} = modelConfig;
+
+    console.log(name)
 
     const initializedModel = await initModel(model, initOptions);
+
+    if (userMessage && Array.isArray(messages)) {
+        messages.find((message) => message.role === 'user').content += userMessage;
+    }
 
     const response = await createCompletion(initializedModel, messages as PromptMessage[], completionOptions);
 
@@ -24,7 +30,12 @@ const start = async (modelName: string) => {
     return response.choices[0].message;
 }
 
-start('Thorin').then((value) => console.log(value.content));
+let story = {}
+// start('Thorin').then((value) => {
+//     story = JSON.parse(value.content);
+//     console.log('STORY:::', story)
+// });
+start('Regin', "Man bites dog").then((value) => console.log(value));
 
 // TODO: Create additional models 
 //  one for sentiment analysis
