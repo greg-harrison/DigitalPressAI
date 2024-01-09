0a3a252e488a3c4f69e18b8bbc7d2ac755d9afd4
Greg <harrison.greg.c@gmail.com>
2024-01-09 13:56:38 -0500

    Model 1 is ready to be tested - Model will need tweaking in the future

--- backend/server.ts
+++ backend/server.ts
@@ -1,25 +1,36 @@
 import { createCompletion, loadModel, PromptMessage } from 'gpt4all';
+import models from './models.json';
 
 const initModel = async (name: string, options = {}) => {
     const model = await loadModel(name, { ...options }); 
-
     return model;
 }
 
-const start = async (modelName: string, message: string) => {
-    const options = { verbose: true };
-    const model = await initModel(modelName, options);
+const start = async (modelName: string) => {
+    const modelConfig = Array.isArray(models.models) ? models.models.find((model) => model.name === modelName) : {};
+
+    if (!modelConfig) throw new Error(`Model ${modelName} not found`);
+
+    const {model, userPrompt, systemPrompt, initOptions, completionOptions} = modelConfig;
+
+    console.log(`Initializing model ${model}... \n initOptions: ${JSON.stringify(initOptions)} \n completionOptions: ${JSON.stringify(completionOptions)}`)
+
+    const initializedModel = await initModel(modelConfig.model, initOptions);
 
     const messages: PromptMessage[] = [
-        { role: 'system', content: 'You are a flavor scientist'},
-        { role: 'user', content: message }
+        { role: 'system', content: systemPrompt },
+        { role: 'user', content: userPrompt },
     ];
 
-    const response = await createCompletion(model, messages);
+    const response = await createCompletion(initializedModel, messages, completionOptions);
 
-    model.dispose();
+    initializedModel.dispose();
 
     return response.choices[0].message;
 }
 
-start('mistral-7b-openorca.Q4_0.gguf', 'write a haiku about hi-chew').then(console.log);
+start('Thorin').then((value) => console.log(value.content));
+
+// TODO: Create additional models 
+//  one for sentiment analysis
+//  one for summarization, etc.
